bb25c89a4cd313562f6950a8d5e2fd0809312cda
i110558 <i110558@B409-1.dei.isep.ipp.pt>
2013-03-14 16:26:22 +0000

    testing madafaker

---
+++ tttt/src/main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author i110558
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x = 0;
+    }
+}
